e83e66f3fbfbc7ca2d4760a20ab7604087f589f6
ducnghu <55783340+ducnghu@users.noreply.github.com>
2020-10-24 23:20:16 +0700

    complete binding MainWindow

--- Food_Recipe/ViewModels/MainViewModel.cs
+++ Food_Recipe/ViewModels/MainViewModel.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
+using System.Configuration;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -12,34 +13,92 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Input;
 using YoutubePlayerLib.Cef;
+using Image = Food_Recipe.Model.Image;
 
 namespace Food_Recipe.ViewModels
 {
     public class MainViewModel : BaseViewModel
     {
+        private const int _smallItem = 6;
+        private const int _largeItem = 12;
+
         #region private 
         private Visibility _isSettingBackgroundVisible = Visibility.Hidden;
         private Visibility _isVideoShown = Visibility.Hidden;
         private Visibility _isAvaShown = Visibility.Visible;
         private bool _isLoaded = true;
         private Uri _videoSource = new Uri("https://www.google.com");
-        private List<Ingredient> _ingredients = new List<Ingredient>();
         private String _videoID = "9i4SKHbhbqk";
+        private bool _isFavorite = false;
         #endregion
 
         public static bool IsShowed = false;
 
+        private bool _isFavoriteRecipes;
+        public bool IsFavoriteRecipes { get => _isFavoriteRecipes; set { _isFavoriteRecipes = value; OnPropertyChanged(); LoadRecipes(IsFavoriteRecipes); } }
+
         private ObservableCollection<Recipe> _recipes;
         public ObservableCollection<Recipe> Recipes { get => _recipes; set { _recipes = value; OnPropertyChanged(); } }
 
-        private int _recipesPage;
-        public int RecipesPage { get => _recipesPage; set { _recipesPage = value; OnPropertyChanged(); } }
+        private int _maxRecipesPerPage;
 
         private ObservableCollection<Recipe> _recipesPerPage;
         public ObservableCollection<Recipe> RecipesPerPage { get => _recipesPerPage; set { _recipesPerPage = value; OnPropertyChanged(); } }
 
-        private Recipe _selectedRecipe;
-        public Recipe SelectedRecipe { get => _selectedRecipe; set { _selectedRecipe = value; OnPropertyChanged(); } }
+        private int _recipesPage;
+        public int RecipesPage { get => _recipesPage;
+            set
+            {
+                _recipesPage = value;
+                OnPropertyChanged();
+                IndexRecipes = $"{_recipesPage}/{Recipes.Count / _maxRecipesPerPage + ((Recipes.Count % _maxRecipesPerPage != 0) ? 1 : 0)}";
+                LoadRecipesPerPage(_maxRecipesPerPage);
+            }
+        }
+
+        private string _indexRecipes;
+        public string IndexRecipes { get => _indexRecipes; set { _indexRecipes = value; OnPropertyChanged(); } }
+
+        private Recipe _showRecipe;
+        public Recipe ShowRecipe { get => _showRecipe; 
+            set { 
+                _showRecipe = value; 
+                OnPropertyChanged();
+                StepPage = 1;
+            } 
+        }
+
+        private Step _nowStep;
+        public Step NowStep { get => _nowStep; 
+            set { 
+                _nowStep = value; 
+                OnPropertyChanged();
+                if (NowStep.Images.Count != 0)
+                {
+                    NowImageStep = NowStep.Images.ToList()[0];
+                }
+                else
+                {
+                    NowImageStep = new Image{ ImageSource = "Data/noimage.png" };
+                }
+            } 
+        }
+
+        private int _stepsPage;
+        public int StepPage { get => _stepsPage; 
+            set { 
+                _stepsPage = value; 
+                OnPropertyChanged();
+                IndexStep = $"{StepPage}/{ShowRecipe.Steps.Count}";
+                NowStep = ShowRecipe.Steps.ToList()[StepPage - 1];
+            } 
+        }
+
+        private string _indexStep;
+        public string IndexStep { get => _indexStep; set { _indexStep = value; OnPropertyChanged(); } }
+
+        private Image _nowImageStep;
+        public Image NowImageStep { get => _nowImageStep; set { _nowImageStep = value; OnPropertyChanged(); } }
 
 
         public ICommand AllRecipeCommand { get; set; }
@@ -63,6 +122,9 @@ namespace Food_Recipe.ViewModels
 
         public ICommand WatchVideoCommand { get; set; }
 
+        public ICommand ClickItemCommand { get; set; }
+        public ICommand FavoriteChanged { get; set; }
+
         //Visibility binding
         public Visibility SettingBackground { get => _isSettingBackgroundVisible; set { _isSettingBackgroundVisible = value; OnPropertyChanged(); } }
         public Visibility VideoVisibility { get => _isVideoShown; set { _isVideoShown = value; OnPropertyChanged(); } }
@@ -75,16 +137,25 @@ namespace Food_Recipe.ViewModels
             if (_isLoaded)
             {
                 _isLoaded = true;
-                SelectedRecipe = DataProvider.Ins.DB.Recipes.ToList()[1];
-                Recipes = new ObservableCollection<Recipe>(DataProvider.Ins.DB.Recipes.ToList());
-                RecipesPage = 2;
-                LoadRecipesPerPage(6);
+
+                IsFavoriteRecipes = false;
+                LoadRecipes(IsFavoriteRecipes);
+                RecipesPage = 1;
             }
 
             //set the command
             AllRecipeCommand = new RelayCommand<object>((prop) => { return true; }, (prop) =>
             {
+                IsFavoriteRecipes = false;
+                LoadRecipes(IsFavoriteRecipes);
+                RecipesPage = 1;
+            });
 
+            FavoriteCommand = new RelayCommand<object>((prop) => { return true; }, (prop) =>
+            {
+                IsFavoriteRecipes = true;
+                LoadRecipes(IsFavoriteRecipes);
+                RecipesPage = 1;
             });
 
             SettingCommand = new RelayCommand<object>((prop) => { return true; }, (prop) =>
@@ -103,6 +174,8 @@ namespace Food_Recipe.ViewModels
             {
                 UserPreferenceWindow userPreferenWindow = new UserPreferenceWindow();
                 userPreferenWindow.ShowDialog();
+                LoadRecipes(IsFavoriteRecipes);
+                RecipesPage = 1;
             });
 
             NewRecipeCommand = new RelayCommand<object>((prop) => { return true; }, (prop) =>
@@ -114,27 +187,88 @@ namespace Food_Recipe.ViewModels
             NextImgStepCommand = new RelayCommand<object>((prop) => { return true; }, (prop) =>
             {
                 //handle next step's image button
+                if (NowStep.Images.Count > 1)
+                {
+                    if (NowImageStep == NowStep.Images.ToList()[NowStep.Images.Count - 1])
+                    {
+                        NowImageStep = NowStep.Images.ToList()[0];
+                    }
+                    else
+                    {
+                        for (int i = 0; i < NowStep.Images.Count - 1; i++)
+                        {
+                            if (NowImageStep == NowStep.Images.ToList()[i])
+                            {
+                                NowImageStep = NowStep.Images.ToList()[i + 1];
+                                break;
+                            }
+                        }
+                    }
+                }
+                
             });
             PrevImgStepCommand = new RelayCommand<object>((prop) => { return true; }, (prop) =>
             {
                 //handle previous step's image button
+                if (NowStep.Images.Count > 1)
+                {
+                    if (NowImageStep == NowStep.Images.ToList()[0])
+                    {
+                        NowImageStep = NowStep.Images.ToList()[NowStep.Images.Count - 1];
+                    }
+                    else
+                    {
+                        for (int i = 0; i < NowStep.Images.Count - 1; i++)
+                        {
+                            if (NowImageStep == NowStep.Images.ToList()[i + 1])
+                            {
+                                NowImageStep = NowStep.Images.ToList()[i];
+                                break;
+                            }
+                        }
+                    }
+                }
             });
             NextStepCommand = new RelayCommand<object>((prop) => { return true; }, (prop) =>
             {
                 //handle next step button
+                if (StepPage == ShowRecipe.Steps.Count)
+                {
+                    StepPage = 1;
+                }
+                else
+                {
+                    StepPage++;
+                }
             });
 
             PrevStepCommand = new RelayCommand<object>((prop) => { return true; }, (prop) =>
             {
                 //handle previous image button
+                if (StepPage == 1)
+                {
+                    StepPage = ShowRecipe.Steps.Count;
+                }
+                else
+                {
+                    StepPage--;
+                }
             });
             NextListCommand = new RelayCommand<object>((prop) => { return true; }, (prop) =>
             {
                 //handle next list button
+                if (RecipesPage < (Recipes.Count / _maxRecipesPerPage + ((Recipes.Count % _maxRecipesPerPage != 0) ? 1 : 0)))
+                {
+                    RecipesPage++;
+                }
             });
             PrevListCommand = new RelayCommand<object>((prop) => { return true; }, (prop) =>
             {
                 //handle next previous button
+                if (RecipesPage > 1)
+                {
+                    RecipesPage--;
+                }
             });
 
             WatchVideoCommand = new RelayCommand<Button>((prop) => { return true; }, (StartVideoBtn) =>
@@ -143,6 +277,17 @@ namespace Food_Recipe.ViewModels
                 OpenVideo();
             });
 
+            ClickItemCommand = new RelayCommand<Recipe>((prop) => { return true; }, (prop) =>
+            {
+                ShowRecipe = prop;
+            });
+
+            FavoriteChanged = new RelayCommand<Recipe>((prop) => { return true; }, (prop) =>
+            {
+                DataProvider.Ins.DB.SaveChanges();
+                LoadRecipes(IsFavoriteRecipes);
+                RecipesPage = RecipesPage;
+            });
             //testing binding
         }
         private void OpenVideo()
@@ -157,9 +302,9 @@ namespace Food_Recipe.ViewModels
         private void LoadRecipesPerPage(int MaxRecipesPerPage)
         {
             RecipesPerPage = new ObservableCollection<Recipe>();
-            for (int i = 0; i < MaxRecipesPerPage && ((RecipesPage - 1) * MaxRecipesPerPage + i) < Recipes.Count; i++)
+            for (int i = 0; i < MaxRecipesPerPage && ((_recipesPage - 1) * MaxRecipesPerPage + i) < Recipes.Count; i++)
             {
-                RecipesPerPage.Add(Recipes[(RecipesPage - 1) * MaxRecipesPerPage + i]);
+                RecipesPerPage.Add(Recipes[(_recipesPage - 1) * MaxRecipesPerPage + i]);
             }
         }
         /**
@@ -177,6 +322,107 @@ namespace Food_Recipe.ViewModels
                 SettingBackground = Visibility.Hidden;
             }
         }
+
+        private void LoadRecipes(bool isFavorite)
+        {
+            var value1 = ConfigurationManager.AppSettings["IsSmallItem"];
+            bool IsSmallItem = bool.Parse(value1);
+            var value2 = ConfigurationManager.AppSettings["SortingType"];
+            string SortingType = value2;
+            var value3 = ConfigurationManager.AppSettings["SortingWay"];
+            bool IsIncrease = bool.Parse(value3);
+
+            if (IsSmallItem == false)
+            {
+                _maxRecipesPerPage = _smallItem;
+            }
+            else
+            {
+                _maxRecipesPerPage = _largeItem;
+            }
+
+            if (isFavorite == true)
+            {
+                Recipes = new ObservableCollection<Recipe>(DataProvider.Ins.DB.Recipes.Where(x => x.IsFavorite == 1).ToList());
+            }
+            else
+            {
+                Recipes = new ObservableCollection<Recipe>(DataProvider.Ins.DB.Recipes.ToList());
+            }
+
+            if (SortingType == "time")
+            {
+                for (int i = 0; i < Recipes.Count - 1; i++)
+                {
+                    for (int j = i + 1; j < Recipes.Count; j++)
+                    {
+                        if (Recipes[i].CreatedTime > Recipes[j].CreatedTime)
+                        {
+                            Recipe tmp = Recipes[i];
+                            Recipes[i] = Recipes[j];
+                            Recipes[j] = tmp;
+                        }
+                    }
+                }
+            }
+            else if (SortingType == "alphabet")
+            {
+                for (int i = 0; i < Recipes.Count - 1; i++)
+                {
+                    for (int j = i + 1; j < Recipes.Count; j++)
+                    {
+                        if (StringCompare(Recipes[i].Name, Recipes[j].Name) == 1)
+                        {
+                            Recipe tmp = Recipes[i];
+                            Recipes[i] = Recipes[j];
+                            Recipes[j] = tmp;
+                        }
+                    }
+                }
+            }
+
+            if (IsIncrease == false)
+            {
+                for (int i = 0; i < Recipes.Count / 2; i++)
+                {
+                    Recipe tmp = Recipes[i];
+                    Recipes[i] = Recipes[Recipes.Count - 1 - i];
+                    Recipes[Recipes.Count - 1 - i] = tmp;
+                }
+            }
+
+        }
+
+        public int StringCompare(string a, string b)
+        {
+            int result = 0;
+            if (a == b)
+            {
+                result = 0;
+            }
+            else
+            {
+                int t = 0;
+                int t_max = (a.Length > b.Length) ? a.Length : b.Length;
+                while (t < t_max && result == 0)
+                {
+                    if (a[t] > b[t])
+                    {
+                        result = 1;
+                    }
+                    else if (a[t] < b[t])
+                    {
+                        result = -1;
+                    }
+                    else
+                    {
+                        t++;
+                    }
+                }
+            }
+            return result;
+        }
+
 
         ////class for testing
         //public class Ingredient
